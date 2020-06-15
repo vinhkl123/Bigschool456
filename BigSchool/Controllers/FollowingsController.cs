39a2d043139f22580a316e5b0b9c2e59eeb6f074
vinh le <lenguyenngocmi@gmaiil.com>
2020-06-15 08:52:58 +0700

    Ẩn nút nhấn Folow và Going

--- BigSchool/Controllers/FollowingsController.cs
+++ BigSchool/Controllers/FollowingsController.cs
@@ -22,7 +22,7 @@ namespace BigSchool.Controllers
         public IHttpActionResult Follow(FollowingDto followingDto)
         {
             var userId = User.Identity.GetUserId();
-            if (_dbContext.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == followingDto.FolloweeId ))
+            if (_dbContext.Followings.Any(f => f.FolloweeId == userId && f.FolloweeId == followingDto.FolloweeId   ))
                 return BadRequest("Following already exists!");
 
             var following = new Following
